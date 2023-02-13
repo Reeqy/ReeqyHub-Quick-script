13c2a718e6f54b9619a76440bfb8cf48b1689d85
Reeqy <109089460+Reeqy@users.noreply.github.com>
2023-02-13 17:35:15 +0000

    Create Reeqylol.lua

---
+++ Reeqylol.lua
@@ -0,0 +1,98 @@
+rconsolename("ReeqyHub")
+
+
+
+rconsoleerr("Loading assets")
+
+wait(0.9)
+
+rconsoleerr("Enabling Chat Spy")
+
+wait(0.5)
+
+rconsoleerr("Loading..")
+
+wait(2)
+
+loadstring(game:HttpGet("https://pastebin.com/raw/Q2atuWxh", true))()
+
+
+rconsoleerr("Loaded!")
+
+rconsoleerr("Checking game...")
+
+if game.PlaceId == 6516141723 then
+     rconsoleerr(" Loading Doors..")
+    
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Loader.lua"))()
+
+end
+
+
+if game.PlaceId == 537413528 then
+     rconsoleerr(" Loading Build A Boat for Treasure..")
+    
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Loader.lua"))()
+
+end
+
+
+if game.PlaceId == 606849621 then
+     rconsoleerr(" Loading Jailbreak..")
+    
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Loader.lua"))()
+
+end
+
+
+
+if game.PlaceId == 2753915549 then
+     rconsoleerr(" Loading Blox Fruits..")
+    
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Larinax999/leak/main/khemtitgamerHUB-BF.lua"))()
+
+end
+
+if game.PlaceId == 1537690962 then
+     rconsoleerr(" Loading Bee Swarm Simulator..")
+    
+loadstring(game:HttpGet("https://www.macrov2-script.xyz/macrov2.lua"))()
+
+end
+
+
+if game.PlaceId == 8737602449 then
+     rconsoleerr(" Loading PLS DONATE..")
+    
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/tzechco-PlsDonateAutofarmBackup/main/autofarm"))()
+
+end
+
+
+if game.PlaceId == 142823291 then
+     rconsoleerr(" Murder Mystery 2..")
+    
+loadstring(game:HttpGet(("https://pastebin.com/raw/mT10xnt7"), true))()
+
+
+end
+
+
+if game.PlaceId == 621129760 then
+    rconsoleerr(" Loading KAT..")
+    
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AkkGamerBoy/Axure-Hub/main/Axure%20Hub"))()
+
+
+end
+
+
+if game.PlaceId == 6284583030 then
+    
+    rconsoleerr(" Loading Pet Sim X")
+    
+loadstring(game:HttpGet("https://milkup.info/script/PetSimulatorX/"))()
+
+
+end
+
